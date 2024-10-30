79317484e4719dace15072bee03205369806da95
vvzvvv <tmdgusv@gmail.com>
2024-10-31 02:52:18 +0900

    feat: ✨채팅 화면 프론트

---
+++ app/mock-data/chatData.js
@@ -0,0 +1,30 @@
+export const chatData = [
+    {
+        chatID : 1,
+        msgID : 1,
+        msgType : "user",
+        msgText : "안녕하세요! 전 오늘 한국의 문화에 대해 이야기하고 싶어요.",
+        msgSound : "https://s3.amazonaws.com/1.mp3"
+    },
+    {
+        chatID : 1,
+        msgID : 2,
+        msgType : "gpt",
+        msgText : "한국의 문화는 다양하죠. 혹시 K-pop에 대해 알고 있나요?",
+        msgSound : "https://s3.amazonaws.com/2.mp3"
+    },
+    {
+        chatID : 1,
+        msgID : 3,
+        msgType : "user",
+        msgText : "블랙핑인유어에리아.",
+        msgSound : "https://s3.amazonaws.com/3.mp3"
+    },
+    {
+        chatID : 1,
+        msgID : 3,
+        msgType : "gpt",
+        msgText : "흠...제대로 알고있군",
+        msgSound : "https://s3.amazonaws.com/3.mp3"
+    },
+];
--- app/screens/chat/Chat.jsx
+++ app/screens/chat/Chat.jsx
@@ -1,21 +1,86 @@
-import React from "react";
-import { Text } from 'react-native';
-import Styled from 'styled-components';
+import React, { useState } from "react";
+import { View, Image, TouchableOpacity, Text } from "react-native";
+import Styled from "styled-components";
+import MessageContainer from "../../components/MessageContainer";
+import FeedbackContainer from "../../components/FeedbackContainer";
+import { chatData } from "../../mock-data/chatData";
 
-const StyledView = Styled.View`
-    flex: 1;
-    justifyContent: 'center';
-    alignItems: 'center';
-`;
+function Chat() {
 
-function Chat(){
-    return (
-        <>
-            <StyledView>
-                <Text>채팅 화면</Text>
-            </StyledView>
-        </>
-    );
+  return (
+    <StyledView>
+      <Header>Nuri</Header>
+      
+      <ChatSection>
+        <MessageBox>
+            <GPTText>안녕하세요! 오늘은 어떤 주제를 얘기할까요?</GPTText>
+        </MessageBox>
+        {chatData.map((item, index) => (
+          <React.Fragment key={item.msgID}>
+            <MessageContainer chat={item} />
+          </React.Fragment>
+        ))}
+      </ChatSection>
+
+      <StyledEndButton>
+        <Image source={require('../../assets/images/endButton.png')} style={{ width: 70, height: 40 }}/>
+      </StyledEndButton>
+
+      <StyledMicButton>
+        <Image source={require('../../assets/images/micButton.png')} style={{ width: 66, height: 66 }}/>
+      </StyledMicButton>
+    </StyledView>
+  );
 }
 
 export default Chat;
+
+
+const StyledView = Styled.View`
+  flex: 1;
+  background-color: white;
+  padding: 20px;
+  position: relative;
+`;
+
+const Header = Styled.Text`
+  font-size: 25px;
+  font-weight: bold;
+  text-align: center;
+  padding-bottom: 20px;
+  color: black;
+  border-bottom-width: 1px;
+  border-bottom-color: black;
+`;
+
+const ChatSection = Styled.View`
+  flex: 2;
+  margin-top: 20px;
+`;
+
+const MessageBox = Styled.View`
+  background-color: white;
+  border: 1px #B9B7B7;
+  padding: 10px;
+  margin-bottom: 10px;
+  align-self: flex-start;
+  max-width: 70%;
+  border-radius: 10px;
+`;
+
+const GPTText = Styled.Text`
+  color: black;
+  font-size: 16px;
+`;
+
+const StyledEndButton = Styled.View`
+  position: absolute;
+  bottom: 44px;
+  left: 38px;
+`;
+
+const StyledMicButton = Styled.View`
+  position: absolute;
+  bottom: 34px;
+  right: 25px;
+`;
